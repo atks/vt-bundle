cd77bd96072a358856fc0ac3905fc9778fd31b53
Adrian Tan <atks@umich.edu>
2015-03-20 14:42:23 -0400

    misc. changes.

--- generate_vt_bundle_grch37_makefile.pl
+++ generate_vt_bundle_grch37_makefile.pl
@@ -71,7 +71,6 @@ mkpath($logDir);
 #programs
 my $vt = "/net/fantasia/home/atks/dev/vt/bundle/bin/vt";
 my $bedtools = "/net/fantasia/home/atks/programs/bedtools2/bin/bedtools";
-my $vcfSort = "/usr/cluster/bin/vcf-sort";
 
 #reference sequence
 my $refFASTAFile = "/net/fantasia/home/atks/ref/genome/hs37d5.fa";
@@ -83,18 +82,86 @@ my $data;
 my $destVCFFile;
 my $destSitesVCFFile;
 
+###################
+#GENCODE Annotation
+###################
+my $srcGTFFile = "/net/fantasia/home/atks/ref/encode/gencode.v19.annotation.gtf.gz";
+my $destGTFFile = "gencode.v19.annotation.gtf.gz";
+
+#sort and bgzip
+$tgt = "$logDir/$destGTFFile.OK";
+$dep = "$srcGTFFile";
+@cmd = ("$binDir/clean_gencode_annotation $srcGTFFile | grep -v \"^#\" | sort -V -k1,1 -k4,5 | bgzip -c > $outputDir/$destGTFFile");
+makeStep($tgt, $dep, @cmd);
+
+#index
+$tgt = "$logDir/$destGTFFile.tbi.OK";
+$dep = "$logDir/$destGTFFile.OK";
+@cmd = ("tabix -pgff $outputDir/$destGTFFile");
+makeStep($tgt, $dep, @cmd);
+
+####################
+#GENCODE CDS regions
+####################
+$srcGTFFile = "/net/fantasia/home/atks/ref/encode/gencode.v19.annotation.gtf.gz";
+my $destBEDFile = "gencode.v19.cds.bed.gz";
+
+#sort and bgzip
+$tgt = "$logDir/$destBEDFile.OK";
+$dep = "$srcGTFFile";
+@cmd = ("zcat $srcGTFFile | grep -v \"^#\" | sort -V -k1,1 -k4,5 | cut -f1,3,4,5 | grep CDS | cut -f1,3,4 | tr '\\t' ':' | uniq | tr ':' '\\t' | bgzip -c > $outputDir/$destBEDFile");
+makeStep($tgt, $dep, @cmd);
+
+#index
+$tgt = "$logDir/$destBEDFile.tbi.OK";
+$dep = "$logDir/$destBEDFile.OK";
+@cmd = ("tabix -pbed $outputDir/$destBEDFile");
+makeStep($tgt, $dep, @cmd);
+
+##############
+#mdust regions
+##############
+$refFASTAFile = "/net/fantasia/home/atks/ref/genome/hs37d5.fa";
+$destBEDFile = "mdust.bed.gz";
+my $dustBEDFile = "$outputDir/$destBEDFile";
+
+#sort and bgzip
+$tgt = "$logDir/$destBEDFile.OK";
+$dep = "$refFASTAFile";
+@cmd = ("$binDir/mdust/mdust $refFASTAFile -c | cut -f1,3,4 | bgzip -c > $outputDir/$destBEDFile");
+makeStep($tgt, $dep, @cmd);
+
+#index
+$tgt = "$logDir/$destBEDFile.tbi.OK";
+$dep = "$logDir/$destBEDFile.OK";
+@cmd = ("tabix -pbed $outputDir/$destBEDFile");
+makeStep($tgt, $dep, @cmd);
+
+###################
+#trf/lobstr regions
+###################
+my $refBEDFile = "/net/fantasia/home/atks/ref/lobstr/lobstr_v3.0.2_hg19_ref.bed";
+$data = "trf.lobstr";
+$destVCFFile = "$data.sites.$ext";
+
+#sort and bgzip
+$tgt = "$logDir/$destVCFFile.OK";
+$dep = "$refFASTAFile";
+@cmd = ("$binDir/convert_lobstr_trf_bed_to_vcf $refBEDFile -r $refFASTAFile | $vt sort - | $vt annotate_regions - -b $dustBEDFile -t DUST -d \"Low complexity sequence annotated by mdust\" -o $outputDir/$destVCFFile 2> $logDir/$data.annotate_regions.log");
+makeStep($tgt, $dep, @cmd);
+
 ################
 #Broad OMNI chip
 ################
 $srcVCFFile = "/net/fantasia/home/atks/data/1000g/working/20131122_broad_omni/Omni25_genotypes_2141_samples.b37.v2.vcf.gz";
-$data = "omni.chip";
-$destVCFFile = "$data.genotypes.$ext";
-$destSitesVCFFile = "$data.sites.$ext";
+$data = "1000G.omni.chip";
+$destVCFFile = "$data.snps.indels.genotypes.$ext";
+$destSitesVCFFile = "$data.snps.indels.sites.$ext";
 
 #remove unecessary fields, normalize variants and removing duplicates
 $tgt = "$logDir/$destVCFFile.OK";
 $dep = "$srcVCFFile";
-@cmd = ("$binDir/clean_omni_chip $srcVCFFile | $vt normalize - -r $refFASTAFile 2> $logDir/$data.normalize.log | $vt mergedups - -o $outputDir/$destVCFFile 2> $logDir/$data.mergedups.log");
+@cmd = ("$binDir/clean_omni_chip $srcVCFFile | $vt normalize - -r $refFASTAFile 2> $logDir/$data.normalize.log | $vt uniq - 2> $logDir/$data.uniq.log | $vt annotate_regions - -b $dustBEDFile -t DUST -d \"Low complexity sequence annotated by mdust\" -o $outputDir/$destVCFFile 2> $logDir/$data.annotate_regions.log");
 makeStep($tgt, $dep, @cmd);
 
 #index file
@@ -125,14 +192,14 @@ makeStep($tgt, $dep, @cmd);
 #Broad Knowledgebase
 ####################
 $srcVCFFile = "/net/fantasia/home/atks/ref/gatk/2.8_b37/NA12878.knowledgebase.snapshot.20131119.b37.vcf.gz";
-$data = "broad.kb";
+$data = "NA12878.broad.kb";
 $destVCFFile = "$data.snps.indels.complex.genotypes.$ext";
 $destSitesVCFFile = "$data.snps.indels.complex.sites.$ext";
 
 #remove unecessary fields, normalize variants and removing duplicates
 $tgt = "$logDir/$destVCFFile.OK";
 $dep = "$srcVCFFile";
-@cmd = ("zcat $srcVCFFile | $vt normalize - -r ~/ref/genome/hs37d5.fa 2> $logDir/$data.normalize.log | $vt mergedups - -o $outputDir/$destVCFFile 2> $logDir/$data.mergedups.log");
+@cmd = ("zcat $srcVCFFile | $vt normalize - -r ~/ref/genome/hs37d5.fa 2> $logDir/$data.normalize.log | $vt uniq - 2> $logDir/$data.uniq.log | $vt annotate_regions - -b $dustBEDFile -t DUST -d \"Low complexity sequence annotated by mdust\" -o $outputDir/$destVCFFile 2> $logDir/$data.annotate_regions.log");
 makeStep($tgt, $dep, @cmd);
 
 ##index file
@@ -159,21 +226,20 @@ $dep = "$logDir/$destSitesVCFFile.OK";
 @cmd = ("$vt index $outputDir/$destSitesVCFFile 2> $logDir/$data.sites.index.log");
 makeStep($tgt, $dep, @cmd);
 
-
 ##################
 #Illumina Platinum
 ##################
-my $srcSNPVCFFile = "/net/fantasia/home/atks/data/1000g/working/20140123_NA12878_Illumina_Platinum/NA12878.wgs.illumina_platinum.20140122.snp.genotypes.vcf.gz";
-my $srcIndelVCFFile = "/net/fantasia/home/atks/data/1000g/working/20140123_NA12878_Illumina_Platinum/NA12878.wgs.illumina_platinum.20140122.indel.genotypes.vcf.gz";
+#ConfidentRegions.bed.gz  
+$srcVCFFile = "/net/fantasia/home/atks/data/platinum_v7/NA12878.vcf.gz";
 
 $data = "NA12878.illumina.platinum";
-$destVCFFile = "$data.5284448variants.genotypes.$ext";
-$destSitesVCFFile = "$data.5284448variants.sites.$ext";
+$destVCFFile = "$data.snps.indels.complex.genotypes.$ext";
+$destSitesVCFFile = "$data.snps.indels.complex.sites.$ext";
 
 #remove unecessary fields, normalize variants and removing duplicates
 $tgt = "$logDir/$destVCFFile.OK";
-$dep = "$srcSNPVCFFile $srcIndelVCFFile";
-@cmd = ("$vt concat $srcSNPVCFFile $srcIndelVCFFile | $vcfSort -c | $vt normalize - -r ~/ref/genome/hs37d5.fa 2> $logDir/$data.normalize.log | $vt mergedups - -o $outputDir/$destVCFFile 2> $logDir/$data.mergedups.log");
+$dep = "$srcVCFFile";
+@cmd = ("$binDir/clean_illumina_platinum $srcVCFFile | $vt normalize - -r ~/ref/genome/hs37d5.fa 2> $logDir/$data.normalize.log | $vt uniq - 2> $logDir/$data.uniq.log | $vt annotate_regions - -b $dustBEDFile -t DUST -d \"Low complexity sequence annotated by mdust\" -o $outputDir/$destVCFFile 2> $logDir/$data.annotate_regions.log");
 makeStep($tgt, $dep, @cmd);
 
 ##index file
@@ -204,13 +270,13 @@ makeStep($tgt, $dep, @cmd);
 #DBSNP (from GATK)
 ##################
 $srcVCFFile = "/net/fantasia/home/atks/ref/gatk/2.3_b37/dbsnp_138.b37.excluding_sites_after_129.vcf.gz";
-$data = "dbsnp";
-$destVCFFile = "$data.13147541variants.sites.$ext";
+$data = "dbSNP138";
+$destVCFFile = "$data.snps.indels.complex.sites.$ext";
 
 #remove unecessary fields, normalize variants and removing duplicates
 $tgt = "$logDir/$destVCFFile.OK";
 $dep = "$srcVCFFile";
-@cmd = ("zcat $srcVCFFile | $vt normalize - -o + -r $refFASTAFile 2> $logDir/$data.normalize.log | $vt mergedups + -o $outputDir/$destVCFFile 2> $logDir/$data.mergedups.log");
+@cmd = ("zcat $srcVCFFile | $vt normalize - -o + -r $refFASTAFile 2> $logDir/$data.normalize.log | $vt uniq + 2> $logDir/$data.uniq.log | $vt annotate_regions - -b $dustBEDFile -t DUST -d \"Low complexity sequence annotated by mdust\" -o $outputDir/$destVCFFile 2> $logDir/$data.annotate_regions.log");
 makeStep($tgt, $dep, @cmd);
 
 #index file
@@ -230,13 +296,13 @@ makeStep($tgt, $dep, @cmd);
 ###########
 $srcVCFFile = "/net/fantasia/home/atks/data/mills/src/mills_indel_genotypes_hg19.vcf";
 $data = "mills.chip";
-$destVCFFile = "$data.158samples.8904indels.genotypes.$ext";
-$destSitesVCFFile = "$data.158samples.8904indels.sites.$ext";
+$destVCFFile = "$data.indels.genotypes.$ext";
+$destSitesVCFFile = "$data.indels.sites.$ext";
 
 #remove unecessary fields, normalize variants and removing duplicates
 $tgt = "$logDir/$destVCFFile.OK";
 $dep = "$srcVCFFile";
-@cmd = ("$binDir/clean_mills_chip $srcVCFFile | $vt normalize - -r $refFASTAFile 2> $logDir/$data.normalize.log | $vt mergedups - -o $outputDir/$destVCFFile 2> $logDir/$data.mergedups.log");
+@cmd = ("$binDir/clean_mills_chip $srcVCFFile | $vt normalize - -r $refFASTAFile 2> $logDir/$data.normalize.log | $vt uniq - 2> $logDir/$data.uniq.log | $vt annotate_regions - -b $dustBEDFile -t DUST -d \"Low complexity sequence annotated by mdust\" -o $outputDir/$destVCFFile 2> $logDir/$data.annotate_regions.log");
 makeStep($tgt, $dep, @cmd);
 
 #index file
@@ -268,12 +334,12 @@ makeStep($tgt, $dep, @cmd);
 ##################
 $srcVCFFile = "/net/fantasia/home/atks/ref/gatk/2.3_b37/Mills_and_1000G_gold_standard.indels.b37.vcf.gz";
 $data = "mills";
-$destVCFFile = "$data.208620indels.sites.$ext";
+$destVCFFile = "$data.indels.sites.$ext";
 
 #remove unecessary fields, normalize variants and removing duplicates
 $tgt = "$logDir/$destVCFFile.OK";
 $dep = "$srcVCFFile";
-@cmd = ("$binDir/clean_mills_1000g_gatk $srcVCFFile | $vt normalize - -r $refFASTAFile 2> $logDir/$data.normalize.log | $vt mergedups - 2> $logDir/$data.mergedups.log | $vt annotate_variants - -r $refFASTAFile 2> $logDir/$data.annotation.log | perl -lane \"{print if (!/CLUMP|MNP/ || /^#/)}\" | $vt view - -o $outputDir/$destVCFFile");
+@cmd = ("$binDir/clean_mills_1000g_gatk $srcVCFFile | $vt normalize - -r $refFASTAFile 2> $logDir/$data.normalize.log | $vt uniq - 2> $logDir/$data.uniq.log | $vt annotate_regions - -b $dustBEDFile -t DUST -d \"Low complexity sequence annotated by mdust\" -o $outputDir/$destVCFFile 2> $logDir/$data.annotate_regions.log");
 makeStep($tgt, $dep, @cmd);
 
 #index file
@@ -291,14 +357,14 @@ makeStep($tgt, $dep, @cmd);
 #############
 #1000 Genomes
 #############
-$srcVCFFile = "/net/fantasia/home/atks/data/1000g/phase1/ALL.wgs.phase1_release_v3.20101123.snps_indels.sites.vcf.gz";
-$data = "1000G";
-$destVCFFile = "$data.snps_indels.sites.$ext";
+$srcVCFFile = "/net/1000g/1000g/release/20130502/supporting/bcf_files/ALL.wgs.phase3_shapeit2_mvncall_integrated_v5.20130502.genotypes.bcf";
+$data = "1000G.v5";
+$destVCFFile = "$data.snps.indels.complex.svs.sites.$ext";
 
 #remove unecessary fields, normalize variants and removing duplicates
 $tgt = "$logDir/$destVCFFile.OK";
 $dep = "$srcVCFFile";
-@cmd = ("$vt normalize $srcVCFFile -r $refFASTAFile 2> $logDir/$data.normalize.log | $vt mergedups - 2> $logDir/$data.mergedups.log | $vt annotate_variants - -r $refFASTAFile 2> $logDir/$data.annotation.log | perl -lane \"{print if (!/CLUMP|MNP/ || /^#/)}\" | $vt view - -o $outputDir/$destVCFFile");
+@cmd = ("$vt view -h $srcVCFFile -s | $vt normalize - -r $refFASTAFile 2> $logDir/$data.normalize.log | $vt uniq - 2> $logDir/$data.uniq.log | $vt annotate_regions - -b $dustBEDFile -t DUST -d \"Low complexity sequence annotated by mdust\" -o $outputDir/$destVCFFile 2> $logDir/$data.annotate_regions.log");
 makeStep($tgt,$dep,@cmd);
 
 #index file
@@ -318,13 +384,13 @@ makeStep($tgt,$dep,@cmd);
 ######################
 $srcVCFFile = "/net/fantasia/home/atks/data/1000g/working/20120208_axiom_genotypes/src/ALL.wex.axiom.20120206.snps_and_indels.genotypes.vcf.gz";
 $data = "affy.exome.chip";
-$destVCFFile = "$data.1249samples.316520variants.genotypes.$ext";
-$destSitesVCFFile = "$data.1249samples.316520variants.sites.$ext";
+$destVCFFile = "$data.snps.indels.complex.genotypes.$ext";
+$destSitesVCFFile = "$data.snps.indels.complex.sites.$ext";
 
 #remove unecessary fields, normalize variants and removing duplicates
 $tgt = "$logDir/$destVCFFile.OK";
 $dep = "$srcVCFFile";
-@cmd = ("$binDir/clean_affy_exome_chip $srcVCFFile | $vt normalize - -r $refFASTAFile 2> $logDir/$data.normalize.log | $vt mergedups - -o $outputDir/$destVCFFile 2> $logDir/$data.mergedups.log");
+@cmd = ("$binDir/clean_affy_exome_chip $srcVCFFile | $vt sort - | $vt normalize - -r $refFASTAFile 2> $logDir/$data.normalize.log | $vt uniq - 2> $logDir/$data.uniq.log | $vt annotate_regions - -b $dustBEDFile -t DUST -d \"Low complexity sequence annotated by mdust\" -o $outputDir/$destVCFFile 2> $logDir/$data.annotate_regions.log");
 makeStep($tgt, $dep, @cmd);
 
 #index file
@@ -350,64 +416,6 @@ $tgt = "$logDir/$destSitesVCFFile.$indexExt.OK";
 $dep = "$logDir/$destSitesVCFFile.OK";
 @cmd = ("$vt index $outputDir/$destSitesVCFFile 2> $logDir/$data.sites.index.log");
 makeStep($tgt, $dep, @cmd);
-
-###################
-#GENCODE Annotation
-###################
-my $srcGTFFile = "/net/fantasia/home/atks/ref/encode/gencode.v19.annotation.gtf.gz";
-my $destGTFFile = "gencode.v19.annotation.gtf.gz";
-
-#sort and bgzip
-$tgt = "$logDir/$destGTFFile.OK";
-$dep = "$srcGTFFile";
-@cmd = ("$binDir/clean_gencode_annotation $srcGTFFile | grep -v \"^#\" | sort -V -k1,1 -k4,5 | bgzip -c > $outputDir/$destGTFFile");
-makeStep($tgt, $dep, @cmd);
-
-#index
-$tgt = "$logDir/$destGTFFile.tbi.OK";
-$dep = "$logDir/$destGTFFile.OK";
-@cmd = ("tabix -pgff $outputDir/$destGTFFile");
-makeStep($tgt, $dep, @cmd);
-
-####################
-#GENCODE CDS regions
-####################
-$srcGTFFile = "/net/fantasia/home/atks/ref/encode/gencode.v19.annotation.gtf.gz";
-my $destBEDFile = "gencode.cds.bed.gz";
-
-#sort and bgzip
-$tgt = "$logDir/$destBEDFile.OK";
-$dep = "$srcGTFFile";
-@cmd = ("zcat $srcGTFFile | grep -v \"^#\" | sort -V -k1,1 -k4,5 | cut -f1,3,4,5 | grep CDS | cut -f1,3,4 | tr '\\t' ':' | uniq | tr ':' '\\t' | bgzip -c > $outputDir/$destBEDFile");
-makeStep($tgt, $dep, @cmd);
-
-#index
-$tgt = "$logDir/$destBEDFile.tbi.OK";
-$dep = "$logDir/$destBEDFile.OK";
-@cmd = ("tabix -pbed $outputDir/$destBEDFile");
-makeStep($tgt, $dep, @cmd);
-
-##############
-#mdust regions
-##############
-$refFASTAFile = "/net/fantasia/home/atks/ref/genome/hs37d5.fa";
-$destBEDFile = "mdust.bed.gz";
-
-#sort and bgzip
-$tgt = "$logDir/$destBEDFile.OK";
-$dep = "$refFASTAFile";
-@cmd = ("$binDir/mdust/mdust $refFASTAFile -c | cut -f1,3,4 | bgzip -c > $outputDir/$destBEDFile");
-makeStep($tgt, $dep, @cmd);
-
-#index
-$tgt = "$logDir/$destBEDFile.tbi.OK";
-$dep = "$logDir/$destBEDFile.OK";
-@cmd = ("tabix -pbed $outputDir/$destBEDFile");
-makeStep($tgt, $dep, @cmd);
-
-############
-#README File
-############
 
 #*******************
 #Write out make file
